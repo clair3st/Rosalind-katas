951ea877e730a77b9c00a5f9f697cf231f051c09
CLAIRE GATENBY <clairejgatenby@gmail.com>
2017-02-23 20:52:07 -0800

    created function to generate profile

---
+++ src/profile.py
@@ -0,0 +1,17 @@
+"""Consensus and Profile."""
+
+def format_data(fasta):
+    """Format fasta file into list of DNA strings."""
+    return [val for idx, val in enumerate(fasta) if idx % 2 != 0]
+
+
+def generate_profile(dna):
+    """Generate profile matrix."""
+    profile = {'A': [0] * len(dna[0]),
+               'C': [0] * len(dna[0]),
+               'G': [0] * len(dna[0]),
+               'T': [0] * len(dna[0])}
+    for strand in dna:
+        for idx, base in enumerate(strand):
+            profile[base][idx] += 1
+    return profile
